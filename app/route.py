b6d01dce538ec97e731931c0eddc493dfe237c0a
AleksSM <aleks-m83@mail.ru>
2023-12-15 21:10:31 +0400

    update

--- app/route.py
+++ app/route.py
@@ -1,25 +1,36 @@
 from app import app
-from flask import request
+from flask import request, redirect, abort, url_for
 from flask import render_template
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 
 @app.route('/')
 def home():
-  return 'This is main page'
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    # abort(401)
+    page_not_found()
 
 
 @app.route('/user')
 def user():
-  return 'This page user'
+    return 'This page user'
 
 
 @app.route('/adverts', methods=['GET', 'POST'])
 def adverts():
-  if request.method != 'GET':
-    return 'error'
-  else:
-    return render_template('base.html')
+    if request.method != 'GET':
+        return 'error'
+    else:
+        return render_template('base.html')
 
 
 @app.route('/adverts/<int:adv_id>')
 def get_adverts():
-  return 'This page for adverts user'
+    return 'This page for adverts user'
