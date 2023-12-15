541040a34059b33b3202bc589e857bcac6b5526e
AleksSM <aleks-m83@mail.ru>
2023-12-15 20:36:05 +0400

    fix

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
 from app import app
+from route import home
 
-# if __name__ == '__main__':
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
