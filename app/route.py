1fc7f1bac00eec73b286aabaf923df144cc9bdee
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-12-15 13:51:04 +0400

    Update route.py

--- app/route.py
+++ app/route.py
@@ -1,4 +1,14 @@
 
 @app.route('/')
-def hello():
-  return 'Hello world'
+def home():
+  return 'This is main page'
+
+
+@app.route('/user')
+def user():
+  return 'This page user'
+
+
+@app.route('/adverts')
+def adverts():
+  return 'This page for adverts user'
