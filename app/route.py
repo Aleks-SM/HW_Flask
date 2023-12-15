fca57d47e12b5cad77bb5603ca3ff8e96e26cb1a
AleksSM <aleks-m83@mail.ru>
2023-12-15 20:36:05 +0400

    fix

--- app/route.py
+++ app/route.py
@@ -1,3 +1,6 @@
+from app import app
+from flask import request
+from flask import render_template
 
 @app.route('/')
 def home():
@@ -9,6 +12,14 @@ def user():
   return 'This page user'
 
 
-@app.route('/adverts')
+@app.route('/adverts', methods=['GET', 'POST'])
 def adverts():
+  if request.method != 'GET':
+    return 'error'
+  else:
+    return render_template('base.html')
+
+
+@app.route('/adverts/<int:adv_id>')
+def get_adverts():
   return 'This page for adverts user'
