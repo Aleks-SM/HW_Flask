68a1a061f6c78448e9400c19300ad02523e72c31
AleksSM <aleks-m83@mail.ru>
2023-12-16 08:58:16 +0400

    add models.py

--- app/models.py
+++ app/models.py
@@ -1,69 +1,25 @@
-import os
-import atexit
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime, ForeignKey, func
 import datetime
+import sqlalchemy as sq
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase
 
-
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", 'secret')
-POSTGRES_USER = os.getenv("POSTGRES_USER", 'user')
-POSTGRES_DB = os.getenv("POSTGRES_DB", 'adv_db')
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", '127.0.0.1')
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", '5431')
-
-
-PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-engine = create_engine(PG_DSN, pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-
-atexit.register(engine.dispose)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class Users(Base):
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(20), unique=True, index=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(100), nullable=False)
-    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-
-    @property
-    def dict(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'description': self.description,
-            'time_of_creation': self.time_of_creation.isoformat(),
-        }
-
-
 class Adverts(Base):
-    __tablename__ = "adverts"
+    __tablename__ = 'adverts'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(40), unique=True, index=True, nullable=False)
-    description: Mapped[str] = mapped_column(String(500), nullable=False)
-    time_of_creation: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
-    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), index=True, nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=50), unique=True, index=True, nullable=False)
+    description = sq.Column(sq.String(length=50), nullable=False)
+    time_create = sq.Column(sq.DateTime, server_default=func.now())
 
     @property
     def dict(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'description': self.description,
-            'time_of_creation': self.time_of_creation.isoformat(),
-        }
-
-    user = relationship("User", backref="user", lazy="dynamic")
-
-
-Base.metadata.create_all(bind=engine)
+        return {'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                'time_create': self.time_create}
