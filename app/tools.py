a5a54097d26b30efe5e5a99af1b1609fb6babdd6
AleksSM <aleks-m83@mail.ru>
2023-12-16 09:42:51 +0400

    update create engine

--- app/tools.py
+++ app/tools.py
@@ -1,6 +1,13 @@
 from schema import SCHEMA_CLASS
 from pydantic import ValidationError
 from errors import HttpError
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from config import TYPE_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB
+
+PG_DSN = f"{TYPE_DB}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = create_engine(PG_DSN)
+Session = sessionmaker(bind=engine)
 
 
 def validate(schema_cls: SCHEMA_CLASS, json_data: dict | list):
