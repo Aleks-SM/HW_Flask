3c790ab14b2cd77b70e0204108e44647b277ef65
AleksSM <aleks-m83@mail.ru>
2023-12-16 10:25:30 +0400

    fix bug

--- app/route.py
+++ app/route.py
@@ -1,45 +1,85 @@
-from app import app
+import flask
+# from app import app
 from errors import HttpError
 from flask import request, redirect, abort, url_for
-from flask import render_template
+from flask import render_template, jsonify, views
+from sqlalchemy.exc import IntegrityError
+from models import Adverts
+from tools import validate, Session
+from schema import CreateAdv, UpdateAdv
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+from flask import Flask
 
-
-# @app.route('/')
-# def home():
-#     return redirect(url_for('login'))
-
-
-@app.route('/login', methods=['GEt', 'POST'])
-def login():
-    # abort(401)
-    # page_not_found(404)
-    # error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password)']
-        return 'Fine'
-    else:
-        # error = 'Invalid username/password'
-        return render_template('login.html')
+app = Flask(__name__)
 
 
-@app.route('/user')
-def user():
-    return 'This page user'
+@app.before_request
+def before_request():
+    session = Session()
+    request.session = session
+
+@app.after_request
+def after_request(response: flask.Response):
+    request.session.close()
+    return response
+
+@app.errorhandler(HttpError)
+def error_handler(error):
+    response = jsonify({"error": error.description})
+    response.status_code = error.status_code
+    return response
+
+def get_adv(adv_id):
+    adv = request.session.get(Adverts, adv_id)
+    if adv is None:
+        raise HttpError(404, 'advertisement not found')
+    return adv
+
+def add_adv(adv: Adverts):
+    try:
+        request.session.add(adv)
+        request.session.commit()
+    except IntegrityError as err:
+        raise HttpError(409, "adv already exists")
+
+class AdvView(views.MethodView):
+
+    @property
+    def session(self) -> Session:
+        return request.session
+
+    def get(self, adv_id):
+        adv = get_adv(adv_id)
+
+        return jsonify(adv.dict)
+
+    def post(self):
+        adv_data = validate(CreateAdv, request.json)
+        adv = Adverts(**adv_data)
+        add_adv(adv)
+        return jsonify({"id": adv.id})
+
+    def patch(self, adv_id):
+        adv = get_adv(adv_id)
+        adv_data = validate(UpdateAdv, request.json)
+        for key, value in adv_data.items():
+            setattr(adv, key, value)
+            add_adv(adv)
+        return jsonify({"id": adv.id})
 
 
-@app.route('/adverts', methods=['GET', 'POST'])
-def adverts():
-    if request.method != 'GET':
-        return 'error'
-    else:
-        return render_template('base.html')
+    def delete(self, adv_id):
+        adv = get_adv(adv_id)
+        self.session.delete(adv)
+        self.session.commit()
+        return jsonify({'status': 'OK'})
 
 
-@app.route('/adverts/<int:adv_id>')
-def get_adverts():
-    return 'This page for adverts user'
+adv_view = AdvView.as_view("adv_view")
+
+
+app.add_url_rule("/adverts/<int:adv_id>", view_func=adv_view, methods=["GET", "PATCH", "DELETE"])
+app.add_url_rule("/adverts", view_func=adv_view, methods=["POST"])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
