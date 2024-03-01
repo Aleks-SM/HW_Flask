829e031e9d8db6253850f2cbfd901e2e4fd056c9
AleksSM <aleks-m83@mail.ru>
2024-03-01 19:41:08 +0400

    fix bugs

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 import os
+import atexit
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, DateTime, func
 import datetime
-
 
 
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", 'secret')
@@ -18,8 +18,9 @@ POSTGRES_PORT = os.getenv("POSTGRES_PORT", '5431')
 PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(PG_DSN, pool_pre_ping=True)
-
 Session = sessionmaker(bind=engine)
+
+atexit.register(engine.dispose)
 
 
 class Base(DeclarativeBase):
@@ -33,7 +34,7 @@ class Adverts(Base):
     title: Mapped[str] = mapped_column(String(40), unique=True, index=True, nullable=False)
     description: Mapped[str] = mapped_column(String(500), nullable=False)
     time_of_creation: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
-    owner: Mapped[str] = mapped_column(String(20), index=True, nullable=False)
+    # owner: Mapped[str] = mapped_column(String(20), index=True, nullable=False)
 
     @property
     def dict(self):
@@ -42,9 +43,7 @@ class Adverts(Base):
             'title': self.title,
             'description': self.description,
             'time_of_creation': self.time_of_creation.isoformat(),
-            'owner': self.owner,
         }
-
 
 
 Base.metadata.create_all(bind=engine)
