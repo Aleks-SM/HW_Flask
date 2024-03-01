c3ebddb7a59fff22becaa321b31a0179f3df85bb
AleksSM <aleks-m83@mail.ru>
2024-03-01 19:41:55 +0400

    fix bugs rows title

--- app/client.py
+++ app/client.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.post('http://127.0.0.1:5000/adverts',
-                         json={"title1": "user1", "description": "qweqweqw"}
+                         json={"title": "user1", "description": "qweqweqw"}
                          )
                          # params={"name": "Jane", "age": "34"},
                          # headers={"token": "some_token"})
@@ -14,7 +14,7 @@ print(response.text)
 # print(response.text)
 
 # response = requests.patch('http://127.0.0.1:5000/adverts/1',
-#                           json={"title": "qwwrre", "description": "qweqweqw", "owner": "sdfsdf"})
+#                           json={"title": "change_title", "description": "qweqweqw", "owner": "sdfsdf"})
 #
 # print(response.status_code)
 # print(response.text)
